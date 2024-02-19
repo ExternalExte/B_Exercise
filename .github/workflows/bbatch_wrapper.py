0b37a544996f8d4baace43b97af003dda315acfb
Exte <externalexte@gmail.com>
2024-02-19 15:36:22 +0900

    fix summary

--- .github/workflows/bbatch_wrapper.py
+++ .github/workflows/bbatch_wrapper.py
@@ -1,4 +1,4 @@
 import subprocess
 print("bbatch_wrapper is started!")
-subprocess.run([f'echo "test_summary="summary ok!" >> $GITHUB_STEP_SUMMARY'], shell=True)
+subprocess.run([f'echo "PROVE_RESULT="summary ok!" >> $GITHUB_OUTPUT'], shell=True)
 print("bbatch_wrapper is exit!")
