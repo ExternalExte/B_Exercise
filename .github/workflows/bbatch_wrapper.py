dfddf875d9b937569f001342c440138b68296832
Exte <externalexte@gmail.com>
2024-02-19 15:54:29 +0900

    update to be able to terminate

--- .github/workflows/bbatch_wrapper.py
+++ .github/workflows/bbatch_wrapper.py
@@ -1,10 +1,14 @@
 import subprocess
 print("bbatch_wrapper is started!")
-subprocess.run([f'echo "PROVE_RESULT="summary ok!" >> $GITHUB_OUTPUT'], shell=True)
 with subprocess.Popen(["/opt/atelierb-free-4.7.1p1/startBB"], encoding='UTF-8',
                       stdin=subprocess.PIPE, stdout=subprocess.PIPE) as bbatch:
   bbatch.stdin.write('help\n')
   bbatch.stdin.flush()
   bbatch.stdout.flush()
   print(bbatch.stdout.readlines())
+  bbatch.stdin.write('quit\n')
+  bbatch.stdin.flush()
+  bbatch.kill()
+result = "summary ok"
+subprocess.run([f'echo "PROVE_RESULT={result}" >> $GITHUB_OUTPUT'], shell=True)
 print("bbatch_wrapper is exit!")
