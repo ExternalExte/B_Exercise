aa5fbd46ee9914d73bff73e029e2ee943b37c428
Externalexte <externalexte@gmail.com>
2024-02-26 15:27:40 +0900

    update autoprove

--- .github/workflows/bbatch_wrapper.py
+++ .github/workflows/bbatch_wrapper.py
@@ -1,13 +1,43 @@
 import subprocess
-print("bbatch_wrapper is started!")
-# with subprocess.Popen(["/opt/atelierb-free-4.7.1p1/startBB"], encoding='UTF-8',
-#                       stdin=subprocess.PIPE, stdout=subprocess.PIPE) as bbatch:
-#   bbatch.stdin.write('help\n')
-#   bbatch.stdin.flush()
-#   bbatch.stdout.flush()
-#   print(bbatch.stdout.readlines())
-#   bbatch.stdin.write('quit\n')
-#   bbatch.stdin.flush()
-result = "summary ok"
-subprocess.run([f'echo "PROVE_RESULT={result}" >> $GITHUB_OUTPUT'], shell=True)
-print("bbatch_wrapper is exit!")
+import os
+import shutil
+import sys
+
+if os.path.exists('bdp'):
+  shutil.rmtree('bdp')
+if os.path.exists('lang'):
+  shutil.rmtree('lang')
+os.makedirs('bdp')
+os.makedirs('lang')
+components = [os.path.basename(file) for file in os.scandir() if file.is_file() and file.name.endswith(('.mch', '.ref', '.imp'))]
+targets = [os.path.splitext(os.path.basename(tg))[0] for tg in sys.argv[1:]]
+
+result = []
+with subprocess.Popen(["/opt/atelierb-free-4.7.1p1/startBB"], stdin=subprocess.PIPE, stdout=subprocess.PIPE , stderr=None) as bbatch:
+  bbatch.stdin.write(f'crp repo bdp lang SOFTWARE\n'.encode())
+  bbatch.stdin.write(f'op repo\n'.encode())
+  for c in components:
+    bbatch.stdin.write(f'af {c}\n'.encode())
+  for tg in targets:
+    bbatch.stdin.write(f'pr {tg} 0\n'.encode())
+  for tg in targets:
+    bbatch.stdin.write(f's {tg}\n'.encode())
+  bbatch.stdin.write('quit\n'.encode())
+  bbatch.stdin.flush()
+  output = bbatch.stdout.readlines()
+  inside = False
+  for line in output: 
+    line = line.decode()
+    if line == 'Beginning interpretation':
+      continue
+    if line.find('End of Printing the status') != -1:
+      inside = False
+    if inside:
+      result.append(line)
+    if line.find('Printing the status of') != -1:
+      inside = True
+print(''.join(result))
+subprocess.run([f'echo "PROVE_RESULT={''.join(result)}" >> $GITHUB_OUTPUT'], shell=True)
+
+shutil.rmtree('bdp')
+shutil.rmtree('lang')
